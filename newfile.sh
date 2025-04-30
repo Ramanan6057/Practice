5e49c791b22988170669506bdc7b918a2c9ad85e
Ramanan <Ramananv2024@gmail.com>
2025-04-30 12:06:45 +0530

    Update newfile.sh

--- newfile.sh
+++ newfile.sh
@@ -1,5 +1,5 @@
 echo "I'm newfile"
 echo "can you see me?"
 date +%Y-%m-%d_%H:%M:%S
-echo "This is the result" > /home/ramanan/jenkinsfiles/result.txt
+echo "This is the result" >> /home/ramanan/jenkinsfiles/result.txt
 echo "--------------------"
